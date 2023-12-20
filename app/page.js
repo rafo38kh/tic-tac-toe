a16a82d41f5be0031da8358f5fcac067231282cd
Rafo Khachikyan <rafokhachikyan@Rafos-MacBook-Pro.local>
2023-12-19 19:32:06 -0800

    added main page 4 buttons states

--- app/page.js
+++ app/page.js
@@ -1,3 +1,31 @@
+"use client";
+import { GameContext } from "@/contexts/GameContextProvider";
+import { useContext, useState } from "react";
+
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  const { playerType, setPlayerType, gameType, setGameType } =
+    useContext(GameContext);
+
+  console.log(playerType, gameType);
+
+  const className = `text-xl ${playerType === "X" ? "bg-yellow" : "bg-blue"}`;
+
+  return (
+    <>
+      <div className="flex gap-4">
+        <button className={className} onClick={() => setPlayerType("X")}>
+          X
+        </button>
+        <button className={className} onClick={() => setPlayerType("O")}>
+          O
+        </button>
+      </div>
+      <div className="flex flex-col">
+        <button onClick={() => setGameType("CPU")}>NEW GAME VS CPU</button>
+        <button onClick={() => setGameType("player")}>
+          NEW GAME VS PLAYER
+        </button>
+      </div>
+    </>
+  );
 }
