1e6acc07b65f63bd7d76dce03f4e83f07321d95f
Rafo Khachikyan <rafokhachikyan@Rafos-MacBook-Pro.local>
2023-12-26 19:22:24 -0800

    merge main branch into this and update tailwind config

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={outfit.className}>
+      <body className={`${outfit.className} bg-darkNavy`}>
         <GameContextProvider>{children}</GameContextProvider>
       </body>
     </html>
